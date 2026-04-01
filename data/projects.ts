5e2cc98011e1087e00181eb58c75d31034a8104d
Smkg911 <alejoespana123@gmail.com>
2026-03-31 22:22:20 -0500

    creando el ts con todos los proyectos pensados en la pagina

---
+++ data/projects.ts
@@ -0,0 +1,27 @@
+export const projects = [
+  {
+    id: "pulmomed",
+    title: "PulmoMed",
+    tagline: "26 Un proyecto de Realidad Virtual y Cancer de Pulmon",
+  },
+  {
+    id: "tutorialvr",
+    title: "TutorialVR",
+    tagline: "25 App de aprendizaje VR para conocer el uso e interaccion de la tecnologia",
+  },
+  {
+    id: "anypost",
+    title: "AnyPost",
+    tagline: "25 Web para automatizar el post en diferentes redes sociales con un solo click",
+  },
+  {
+    id: "goldengymapp",
+    title: "GoldenGymApp",
+    tagline: "25 Web para la gestion de entrada y analisis de datos de un Gym",
+  },
+  {
+    id: "fullhouse",
+    title: "FullHouse",
+    tagline: "25 web de gestion de inmobiliarias",
+  }
+];
