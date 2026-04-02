ea0b95daf71bbfe497b4febb948a848b3a26cea7
Smkg911 <alejoespana123@gmail.com>
2026-04-01 19:05:20 -0500

    Agregando el texto correspondiente a la page de Info

---
+++ data/info.ts
@@ -0,0 +1,79 @@
+export const timeline = [
+  {
+    side: "right",
+    date: "Diciembre 2019",
+    description: "Me Graduo de la academia de Cambride adquiriendo un nivel de ingles B2",
+  },
+  {
+    side: "left",
+    date: "Noviembre 2023",
+    description: "Me Graduo del bachillerato obteniendo mi titulo",
+  },
+  {
+    side: "right",
+    date: "Febrero 2024",
+    description: "Comienza mi recorrido en la carrera de Ingenieria de Software",
+  },
+  {
+    side: "left",
+    date: "Octubre 2024",
+    description: "Comienza el Proyecto PulmoMed, ingresando a semilleros de investigacion",
+  },
+  {
+    side: "right",
+    date: "Abril 2025",
+    description: "Comienzo a trabajar como desarrollador de Software y gestor de proyectos en un laboratorio de realidad virtual",
+  },
+  {
+    side: "left",
+    date: "Octubre 2025",
+    description: "Desarrollo una web para un gimnasio para hcer analitica de datos, siendo un exito y logrando su venta",
+  },
+];
+
+export const skills = {
+  back: {
+    title: "Habilidades Back",
+    languages: ["Python", "Java", "C#"],
+    frameworks: ["Django", "FastApi", "SpringBoot"],
+  },
+  front: {
+    title: "Habilidades Front",
+    languages: ["JavaScript", "HTML", "Css"],
+    frameworks: ["Angular", "Tailwind", "Next.Js"],
+  },
+  otros: {
+    title: "Otros",
+    items: ["Unity", "Postgress", "Mongo", "AWS", "AZURE", "Git", "Github", "Docker"],
+  },
+};
+
+export const testimonials = [
+  {
+    name: "Luis Carlos Revelo Tovar",
+    text: "Aqui va el Testimonio",
+  },
+  {
+    name: "Daniel Fernando Arteaga",
+    text: "Aqui va el Testimonio",
+  },
+  {
+    name: "Oscar Andrés Osorio",
+    text: "Aqui va el Testimonio",
+  },
+];
+
+export const contacts = [
+  { name: "Luciana Cuenca", relation: "Portafolio" },
+  { name: "Carlos Burbano", relation: "Portafolio" },
+  { name: "Matheu Ruales", relation: "Portafolio" },
+  { name: "Bayron Felipe", relation: "Portafolio" },
+  { name: "Luis Esteban Castillo", relation: "Portafolio" },
+  { name: "Daniel Fernando Arteaga", relation: "Portafolio" },
+];
+
+export const contactMessage = {
+  text: "Gracias por llegar hasta aquí, si crees que puedo aportar valor a tu equipo o simplemente tienes una pregunta por hacerme, no dudes en escribirme y ¡haré todo lo posible para responderle!",
+  email: "alejcespana123@gmail.com",
+  location: "Pasto - Nariño - Colombia",
+};
